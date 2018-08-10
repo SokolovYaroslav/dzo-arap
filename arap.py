c4761941bcbabad4e86c833e5fc4e0f5c96f8298
Iaroslav Sokolov <sokolov.yas@gmail.com>
2018-08-10 14:15:27 +0300

    Intermediate commit

--- arap.py
+++ arap.py
@@ -15,5 +15,17 @@ def compute_mask(mask, orig, int width, int height, int tolerance):
 def clear(orig, data, width, height):
     data = orig[0, 0, :]
 
-def dot(double * homography, float x, float y, float &rx, float &ry):
+def dot(homography, x, y, rx, ry):
+    H = homography.dot(np.array([x, y, 1]))
+    rx = H[0] / H[2]
+    ry = H[1] / H[2]
+    return rx, ry
+
+def store(std::map<int,int> &left, std::map<int,int> &right, int x, int y):
+    """
+    Vrode o4evidno
+    """
+    pass
+
+def points(std::map<int, int> &left, std::map<int,int> &right, bool swap, int x0, int y0, int x1, int y1):
     
