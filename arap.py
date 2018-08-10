448cc0c126b76f918cd0771960b2857303ed2df2
Egor Spirin <egor@spirin.tech>
2018-08-10 14:12:44 +0300

    implement compute_mask

--- arap.py
+++ arap.py
@@ -1,6 +1,7 @@
 import numpy as np
+from collections import deque
 
-def compute_mask(mask, orig, int width, int height, int tolerance):
+def compute_mask(mask: np.ndarray, orig: np.ndarray, width: int, height: int, tolerance: int) -> None:
     """
     mask: np.ndarray of bools with shape (height, width)
     orig: np.ndarray of ints with shape (height, width, 3) (RGB channels)
@@ -10,10 +11,30 @@ def compute_mask(mask, orig, int width, int height, int tolerance):
 
     Returns: None, but should fill mask array
     """
-    pass
+    empty = orig[0:0:] & 255
+    # bounds
+    lo = empty - tolerance
+    up = empty + tolerance
+    # queue
+    queue = deque()
+    queue.append((0, 0))
+    d = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    # closed
+    closed = np.full((height, width), False)
+    while len(queue) != 0:
+        x, y = queue.popleft()
+        if x < 0 or x >= width or y < 0 or y >= height:
+            continue
+        if closed[y][x]:
+            continue
+        px = orig[y:x:] & 255
+        if all([px[i] >= lo[i] and px[i] <= up[i] for i in len(px)]):
+            mask[y][x] = True
+            for dx, dy in d:
+                queue.append((x + dx, y + dy))
 
-def clear(orig, data, width, height):
-    data = orig[0, 0, :]
+# def clear(orig, data, width, height):
+    # data = orig[0, 0, :]
 
-def dot(double * homography, float x, float y, float &rx, float &ry):
+# def dot(double * homography, float x, float y, float &rx, float &ry):
     
