9adb2d67ac13bf8efaa42c2a73da5781ec4dc75b
Spirin Egor <egor@spirin.tech>
2018-08-12 15:47:52 +0300

    add pragma flags to cpp

--- arap.cpp
+++ arap.cpp
@@ -1,3 +1,6 @@
+#pragma GCC optimize("O3,unroll-loops,Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
 #include <cstdio>
 #include <cstring>
 #include <cmath>
