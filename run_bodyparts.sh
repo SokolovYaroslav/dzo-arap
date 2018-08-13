38a6dfc8c11d31bbb2723fe34e1f90d9dbcd8237
Iaroslav Sokolov <sokolov.yas@gmail.com>
2018-08-13 21:31:02 +0300

    Edit bodypart script

--- run_bodyparts.sh
+++ run_bodyparts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-make && python main.py --path assets/1.png \
-                       --mask masks/1_full_mask.png \
+make && python main.py --path assets/masked_001.png \
+                       --mask masks/001_full_mask.png \
                        --grid True --save_mask False --enumerate True \
                        --box_size 32 --control_weight 100000
