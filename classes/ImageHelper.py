43c7ed955216250e1e2798988c2fb690f6ee1492
Iaroslav Sokolov <sokolov.yas@gmail.com>
2018-08-13 14:03:21 +0300

    Implement bind between bodyparts and body

--- classes/ImageHelper.py
+++ classes/ImageHelper.py
@@ -13,6 +13,7 @@ class ImageHelper:
     HANDLE_RADIUS = 5
 
     def __init__(self, cw, args):
+        self.BINDER_MASK_THRESHOLD = 0.09
         self.cw = cw
         self._canvas = None
 
@@ -87,13 +88,52 @@ class ImageHelper:
         return self._orig
 
     def compute_background(self, args):
+        def get_binder_mask(part_mask, body_mask):
+            def get_contour(bool_mask):
+                mask_rgb = (bool_mask * 255)[:, :, np.newaxis][:, :, [0,0,0]]
+                #MEGA KOSTYL
+                cv2.imwrite('tmp.png', mask_rgb)
+                mask_rgb = cv2.imread('tmp.png')
+
+                imgray = cv2.cvtColor(mask_rgb, cv2.COLOR_BGR2GRAY)
+                _, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY)
+                _, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+                return contours[0].reshape(-1,2)[:, [1,0]]
+
+            part_coords =  np.argwhere(part_mask)
+            full_mask = part_mask + body_mask
+
+            body_cont = get_contour(body_mask)
+            full_cont = get_contour(full_mask)
+
+            #KOSTYL: set(body_cont) - set(full_cont)
+            tmp_cont = np.zeros_like(full_mask, dtype=np.bool)
+            tmp_cont[body_cont[:, 0], body_cont[:, 1]] = True
+            tmp_cont[full_cont[:, 0], full_cont[:, 1]] = False
+
+            border = np.argwhere(tmp_cont)
+            
+            #shape: (num border points, num part_coords)
+            part_dists = np.sqrt((border ** 2).sum(axis=1, keepdims=1) + (part_coords ** 2).sum(axis=1) \
+                                -2 * border.dot(part_coords.T)).sum(axis=0)
+            #map to [0,1]
+            part_dists = (part_dists - part_dists.min())
+            part_dists /= part_dists.max()
+
+            binder_coords = part_coords[part_dists < self.BINDER_MASK_THRESHOLD, :]
+            binder_mask = np.zeros_like(part_mask, dtype=np.bool)
+            binder_mask[binder_coords[:, 0], binder_coords[:, 1]] = True
+            return binder_mask
+            
         self._background = self._orig * (1 - self._mask[:, :, np.newaxis])
-        #################
-        #SOME INPAINTING#
-        #################
+        #######################
+        #TODO: SOME INPAINTING#
+        #######################
         if args.bodypart_mask is not None:
             part_only_mask = cv2.imread(args.bodypart_mask, 0).astype(np.bool)
             body_only_mask = (self._mask * (1 - part_only_mask)).astype(np.bool)
+            binder_mask = get_binder_mask(part_only_mask, body_only_mask)
+            body_only_mask += binder_mask
             self._background[body_only_mask] = self._orig[body_only_mask]
             self._mask = part_only_mask
         
