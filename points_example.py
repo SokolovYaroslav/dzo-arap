fce6af981cb3e2b65645d71c04ae8d60bd904bfe
Spirin Egor <egor@spirin.tech>
2018-08-19 21:25:39 +0300

    add algo to find corresponding points

--- points_example.py
+++ points_example.py
@@ -5,4 +5,5 @@ to_num = '1'
 dir = 'morphing/'
 
 get_points((dir+'{}.png'.format(from_num), dir+'m{}.png'.format(from_num), dir+'{}_keypoints.json'.format(from_num)),
-           (dir+'{}.png'.format(to_num),dir+'m{}.png'.format(to_num), dir+'{}_keypoints.json'.format(to_num)))
+           (dir+'{}.png'.format(to_num),dir+'m{}.png'.format(to_num), dir+'{}_keypoints.json'.format(to_num)),
+           contour_pairs=True)
